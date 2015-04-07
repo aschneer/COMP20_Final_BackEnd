d5c3a9805d1a2dcd7fbc31b82f264164f88583a9
hunter- <hunter.wapman@gmail.com>
2015-04-07 16:23:54 -0400

    tryna get heroku workin

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var validator = require('validator');
 
 var app = express();
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
