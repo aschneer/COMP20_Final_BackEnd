99e937d00918487ae1b82df995069dd9aa779fb6
hunter- <hunter.wapman@gmail.com>
2015-04-22 11:20:53 -0400

    pushing untested stuff so whole team can see current progress

--- index.js
+++ index.js
@@ -52,16 +52,37 @@ app.get('/', function(req, res, next) {
 app.post('/sendOffer', function(req, res, next) {
     var data = req.body;
     if (data.hasOwnProperty('provider') && data.hasOwnProperty('food') && data.hasOwnProperty('address') && data.hasOwnProperty('when')) {
+        var toInsert = {'provider': data.provider, 'food': data.food, 'address': data.address, 'when': data.when};
+
+
+
         // Insert
 
-        // db.collection('offers', function(er, offers) {
-        //     assert.equal(null, er);
+        // UNTESTED AT ALL; QUANTITY HANDLING WOULD MAKE THIS SIMPLER
+        db.collection('offers', function(er, offers) {
+            assert.equal(null, er);
+            offers.findOne(toInsert, function(err, cursor) {
+                assert.equal(null, err);
 
-        // });
-        res.status(200);
-        res.send("so much steve");
+                // additional attributes for server manipulation but not order identification
+                toInsert.claimed = false;                
+                // maybe add quantity here?
+
+                if (!cursor) {
+                    offers.insert(toInsert, function(errr, result) {
+                        assert.equal(errr, null);
+                        assert.equal(1, result.result.n);
+                        assert.equal(1, result.ops.length);
+                    });
+                } else {
+                    offers.update(cursor, toInsert, function(errr) {
+                        assert.equal(errr, null);                      
+                    });
+                }
+            });
+        });
+        res.sendStatus(200);
     } else {
-        console.log('not it all');
         res.send('bad sendOffer POST yo');
     }
 });
@@ -70,6 +91,73 @@ app.post('/sendOffer', function(req, res, next) {
 app.post('/claimOffer', function(req, res, next) {
     var data = req.body;
     if (data.hasOwnProperty('_id') && data.hasOwnProperty('login')) {
+        var toInsert;
+        toInsert.login = data.login;
+
+        // UNTESTED AT ALL FROM HERE DOWN
+
+        // get _id'ed element
+        db.collection('offers', function(er, offers) {
+            assert.equal(er, null);
+            offers.findOne({'_id': data._id}, function(err, offer) {
+                if (err) {
+                    res.send('bad offer yo')
+                } 
+
+                toInsert.provider = offer.provider;
+                toInsert.address = offer.address;
+                toInsert.food = offer.food;
+                toInsert.address = offer.when;
+
+                if (offer.hasOwnProperty('quantity')) {
+                    if (offer.quantity >= 1) {
+                        // UPDATE IT HERE; needs logic
+                        var newQuantity = offer.quantity - 1;
+                        offers.update({'_id': data._id}, {'quantity': newQuantity}, function(errr){
+                            assert.equal(errr, null);
+                        });
+
+                    } else if (offer.quantity === 1) {
+                        // remove it here; needs logic
+                        offers.remove({'_id': data._id}, function(errr) {
+                            assert.equal(errr, null);
+                        });
+                    } else {
+                        res.send('bad offer yo');
+                    }
+
+                } else {
+                    offers.remove({'_id': data._id}, function(errr) {
+                        assert.equal(errr, null);
+                    });
+                }
+            });
+        });
+
+        db.collection('offers', function(er, offers) {
+            assert.equal(null, er);
+            offers.findOne(toInsert, function(err, cursor) {
+                assert.equal(null, err);
+
+                // additional attributes for server manipulation but not order identification
+                toInsert.claimed = false;                
+                // maybe add quantity here?
+
+                if (!cursor) {
+                    offers.insert(toInsert, function(errr, result) {
+                        assert.equal(errr, null);
+                        assert.equal(1, result.result.n);
+                        assert.equal(1, result.ops.length);
+                    });
+                } else {
+                    offers.update(cursor, toInsert, function(errr) {
+                        assert.equal(errr, null);                      
+                    });
+                }
+            });
+        });
+
+
         res.sendStatus(200);
     } else {
         res.send('bad claimOffer POST yo');
